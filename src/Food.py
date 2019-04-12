bbb1daf76a4d20d4d494301b556e47cad6e023e1
Navya RS <navya.rs@myntra.com>
2019-04-12 23:16:39 +0530

    removed the class Food and retained its methids and variables as normal functions in the file Food.py

--- src/Food.py
+++ src/Food.py
@@ -3,25 +3,19 @@ from numpy.random import randint
 from Point import Point
 from Constants import *
 
-class Food:
-    def __init__(self):
-        self.foodList = []
-        self.createFood(maximumFood)
+foodList = []
 
-    def returnFoodList(self):
-        return self.foodList    
+def createFood(n):
+    for i in range(n):
+        x = randint(1, gridSize-1)
+        y = randint(1, gridSize-1)
+        foodList.append(Point(x, y))
 
-    def addFoodToList(self, pointList):
-        for i in pointlist:
-            self.foodList.append(i)
+def addFoodToList(pointList):
+    for i in pointList:
+        foodList.append(i)
 
-    def createFood(self, n):
-        for i in range(n):
-            x = randint(1, gridSize-1)
-            y = randint(1, gridSize-1)
-            self.foodList.append(Point(x, y))
-
-    def eatFood(self, food):
-        self.foodList.remove(food)
-        if(len(self.foodList) < maximumFood):
-            self.createFood(maximumFood - len(self.foodList))
+def eatFood(food):
+    foodList.remove(food)
+    if(len(foodList) < maximumFood):
+        createFood(maximumFood - len(foodList))
