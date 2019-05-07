4019497c9e392a3566a6a1ce85b8ae9458155a7e
keerthanss <keerthanss9@gmail.com>
2019-05-07 13:19:58 +0530

    Make user's snake always black
    - Move ScalingFactor into frunGame's args
    - Move foodList display and g.move() call out of snakes' loop

--- src/Graphics-Env.py
+++ src/Graphics-Env.py
@@ -8,7 +8,6 @@ import numpy as np
 from Action import *
 from Constants import *
 
-scalingFactor = 9  # Scaling the size of the grid
 pygame.init()  # Initializes all pygame modules
 
 
@@ -40,21 +39,22 @@ def manual_action(g,event):
     else:
         defaultaction = g.snakes[0].findDirection(g.snakes[0].head, g.snakes[0].joints[0])
 
+    actionsList[0] = defaultaction
+    user_permissible_actions = g.snakes[0].permissible_actions()
     if event.type == pygame.KEYDOWN:
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] and Action.RIGHT in user_permissible_actions:
             actionsList[0] = Action.RIGHT
-        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_LEFT] and Action.LEFT in user_permissible_actions:
             actionsList[0] = Action.LEFT
-        elif keys[pygame.K_UP]:
+        elif keys[pygame.K_UP] and Action.TOP in user_permissible_actions:
             actionsList[0] = Action.TOP
-        elif keys[pygame.K_DOWN]:
+        elif keys[pygame.K_DOWN] and Action.DOWN in user_permissible_actions:
             actionsList[0] = Action.DOWN
-    else:
-        actionsList[0] = defaultaction
+
     return actionsList
 
 
-def runGame():
+def runGame(play=True, scalingFactor = 9):  # Scaling the size of the grid):
     g = Game.Game(numberOfSnakes, gridSize, globalEpisodeLength)  # Instantiating an object of class Game
     width = scalingFactor * gridSize
     height = scalingFactor * gridSize
@@ -66,6 +66,8 @@ def runGame():
     red = (255, 0, 0)
     green = (0, 255, 0)
     colors = np.random.randint(0, 256, size=[numberOfSnakes, 3])
+    if play: # user interacts with the agents
+        colors[0] = black # player's snake is always black
     crashed = False
     win = pygame.display.set_mode((scalingFactor * gridSize, scalingFactor * gridSize))  # Game Window
     screen = pygame.Surface((gridSize+1, gridSize+1))  # Grid Screen
@@ -79,27 +81,24 @@ def runGame():
         screen.fill(white)
         #draw the walls
         pygame.draw.lines(screen, black, True, [(0,0), (0,gridSize), (gridSize, gridSize), (gridSize, 0)])
+        # The below loop draws all the food particles as points.
+        for p in Food.foodList:
+            pygame.draw.line(screen, green, to_pygame(p), to_pygame(p), 1)  # Drawing all the food points
 
+        # This is for drawing the snake and also the snake's head is colored red
         for idx in range(numberOfSnakes):
-            s = g.snakes[idx]
-            body = [s.head]
-            body.extend(s.joints)
-            body.append(s.end)
-            for p in Food.foodList:
-                pygame.draw.line(screen, green, to_pygame(p), to_pygame(p), 1)  # Drawing all the food points
-            """The above loop draws all the food particles as points."""
+            body = g.snakes[idx].getBodyList()
             for i in range(len(body) - 1):
                 pygame.draw.line(screen, colors[idx], to_pygame(body[i]), to_pygame(body[i + 1]), 1)
             pygame.draw.line(screen, red, to_pygame(body[0]), to_pygame(body[0]), 1)
-            """This is for drawing the snake and also the snake's head is colored red"""
 
-            actionsList = manual_action(g, event)
-            """
-            actionsList = rl_agent(g)
-            Can also add an if condition here to have one player and one agent
-            and then append the two lists obtained, to pass it to move method
-            """
-            g.move(actionsList)
+        actionsList = manual_action(g, event)
+        """
+        actionsList = rl_agent(g)
+        Can also add an if condition here to have one player and one agent
+        and then append the two lists obtained, to pass it to move method
+        """
+        g.move(actionsList)
         win.blit(pygame.transform.scale(screen, win.get_rect().size), (0, 0)) # Transforms the screen window into the win window
         for idx in range(numberOfSnakes):
             draw_text(win, "Snake" + str(idx) + "  " + str(g.snakes[idx].score), font_size, pos_score_x * (idx + 1), pos_score_y,black) #Displaying score
@@ -108,4 +107,5 @@ def runGame():
     pygame.quit()
 
 
-runGame()
+if __name__ == '__main__':
+    runGame()
