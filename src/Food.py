356a6bd3a251f85dfbd5a7df16c02d20a05c2a20
navyars <navyars82@gmail.com>
2019-05-11 20:17:43 +0530

    add description for food class and its methods

--- src/Food.py
+++ src/Food.py
@@ -1,3 +1,8 @@
+''' This file contains the Food class which has
+methods that create food points, add new food 
+points created to the foodlist, and remove the 
+food point from the foodlist once the point is eaten '''
+
 from numpy.random import randint
 
 from Point import Point
@@ -9,6 +14,9 @@ class Food:
         self.foodList = []
         self.createFood(maximumFood, snakes)
 
+    ''' This method spawns specified number of food points in the 
+    grid at random positions. It also ensures that the food points
+    created are not overlapping '''
     def createFood(self, n, snakes=[]):
         occupiedPoints = []
         for snake in snakes:
@@ -25,10 +33,16 @@ class Food:
                     self.foodList.append(p)
                     break
 
+    ''' This method is used to update the foodlist either when new 
+    food points get added  or when the dead snake's body points are
+    converted to food points '''
     def addFoodToList(self, pointList):
         for p in pointList:
             self.foodList.append(p)
 
+    ''' This method deletes the food from the foodlist once it has
+    been eaten. It also maintains a minimum number of food points in
+    the grid at an instance '''
     def eatFood(self, food, snakes=[]):
         for i, f in enumerate(self.foodList):
             if f == food:
